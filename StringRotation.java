d8fde74b69195e080f583d10285fe14a0d64b215
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-05-28 15:20:25 +0530

    Solve String Rotation
    This is problem 1.9 in CTCI.

---
+++ StringRotation.java
@@ -0,0 +1,15 @@
+public class StringRotation {
+    public static boolean checkRotation(String s2, String s1) {
+        String s2s2 = s2 + s2;
+        return s1.length() == s2.length() && isSubstring(s1, s2s2);
+    }
+
+    public static boolean isSubstring(String s1, String s2) {
+        return s2.contains(s1);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "waterbottle", s2 = "erbottlewat";
+        System.out.println(checkRotation(s2, s1));
+    }
+}
