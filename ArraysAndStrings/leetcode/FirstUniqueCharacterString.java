d344305199eb69c817a9930df8be97ced499ed92
saubhik <saubhik.mukherjee@gmail.com>
2021-06-12 10:42:08 +0530

    Update FirstUniqueCharacterString.java

--- ArraysAndStrings/leetcode/FirstUniqueCharacterString.java
+++ ArraysAndStrings/leetcode/FirstUniqueCharacterString.java
@@ -15,8 +15,7 @@ class Solution {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         
         for (int i = 0; i < s.length(); ++i)
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);        
         
         for (int i = 0; i < s.length(); ++i)
             if (map.get(s.charAt(i)) == 1)
