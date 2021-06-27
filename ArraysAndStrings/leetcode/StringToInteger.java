9ed3a67a7711a749eec715b3c07219023377fcbb
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-06-27 12:51:03 +0530

    Solve String To Integer

--- ArraysAndStrings/leetcode/StringToInteger.java
+++ ArraysAndStrings/leetcode/StringToInteger.java
@@ -1,34 +1,69 @@
-class Solution {
-    public int myAtoi(String s) {
-        boolean begin = true, negative = false;
-        long ans = 0, max = (1 << 31) - 1, min = -(1 << 31);
-        for (char ch : s.toCharArray()) {
-            System.out.println(negative);
-            if (begin) {
-                switch (ch) {
-                    case ' ': continue;
-                    case '-': negative = true;
-                    case '+': {
-                        begin = false;
-                        continue;
-                    }
-                }
-            }
-            if ('0' <= ch && ch <= '9') {
-                ans = 10 * ans + (ch - '0');
-                if (begin) begin = false;
-            } else return negative ? (int) -ans : (int) ans;
-        }
-        if (negative) ans *= -1;
-        return (int) Math.max(min, Math.min(max, ans));
-    }
-}
-
 public class StringToInteger {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        // System.out.println(s.myAtoi("  -91283472332"));
-        // System.out.println(s.myAtoi("3.14159"));
-        System.out.println(s.myAtoi("  -0012a42"));
+    public int myAtoi(String s) {
+        int ans;
+        int digit;
+        boolean begin;
+        boolean negative;
+        boolean sign;
+        
+        ans = 0;
+        begin = true;
+        negative = false;
+        sign = false;
+        
+        for (char ch : s.toCharArray()) {
+            if (begin && ch == ' ') {
+                continue;
+            }
+            
+            if (!sign && begin && ch == '-') {
+                negative = true;
+                sign = true;
+                begin = false;
+                continue;
+            }
+            
+            if (!sign && begin && ch == '+') {
+                sign = true;
+                begin = false;
+                continue;
+            }
+            
+            if (Character.isDigit(ch)) {
+                if (begin) {
+                    begin = false;
+                }
+                
+                if (ans < Integer.MIN_VALUE / 10) {
+                    return Integer.MIN_VALUE;
+                }
+                
+                if (ans > Integer.MAX_VALUE / 10) {
+                    return Integer.MAX_VALUE;
+                }
+                
+                ans *= 10;       
+                digit = ch - '0';
+                
+                if (negative) {
+                    if (ans < Integer.MIN_VALUE + digit) {
+                        return Integer.MIN_VALUE;
+                    }
+                    
+                    ans -= digit;
+                } else {
+                    if (ans > Integer.MAX_VALUE - digit) {
+                        return Integer.MAX_VALUE;
+                    }
+                    
+                    ans += digit;
+                }
+                
+            } else {
+               break;
+            }
+        }
+        
+        return ans;
     }
 }
