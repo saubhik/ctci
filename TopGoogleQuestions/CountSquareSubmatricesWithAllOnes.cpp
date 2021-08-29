9a413f61d9b0d71e362c31c13f5be0d4e3c54b88
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-08-28 20:35:54 -0400

    solve count square submatrices with all ones

---
+++ TopGoogleQuestions/CountSquareSubmatricesWithAllOnes.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int countSquares(std::vector<std::vector<int>>& matrix) {
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        int res = 0;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i && j && matrix[i][j]) {
+                    matrix[i][j] += std::min({
+                        matrix[i-1][j],
+                        matrix[i-1][j-1],
+                        matrix[i][j-1]
+                    });
+                }
+                res += matrix[i][j];
+            }
+        }
+        
+        return res;
+    }
+};
